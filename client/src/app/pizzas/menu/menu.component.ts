51aac31c7961cabc048e9b61a062f47d7e1907aa
Ivaylo Doynov <ivaylodoynov@Ivaylos-MacBook-Air.local>
2021-08-06 13:33:03 +0200

    update cart service

--- client/src/app/pizzas/menu/menu.component.ts
+++ client/src/app/pizzas/menu/menu.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { IPizza } from '../../core/models/pizza.model';
+import { IErrors } from '../../core/models/errors.model';
+import { AuthService } from '../../core/services/auth.service';
+import { CartsService } from '../../core/services/carts.service';
+import { PizzasService } from '../../core/services/pizzas.service';
 
 @Component({
   selector: 'app-menu',
@@ -7,9 +14,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
-  constructor() { }
+  query: string;
+  page: number;
+  totalPages: number;
+  pizzas: Array<IPizza>;
+  errors: IErrors = { errors: {} };
 
-  ngOnInit(): void {
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private authService: AuthService,
+    private cartsService: CartsService,
+    private pizzasService: PizzasService
+  ) {
+    this.query = this.route.snapshot.paramMap.get('query') ?? '';
+    this.page = Number(this.route.snapshot.paramMap.get('page')) ?? 1;
   }
 
+  ngOnInit(): void {
+    if (this.page == 0) {
+      this.page = 1;
+    }
+
+    this.pizzasService.search(this.query).subscribe(pizzas => {
+      this.pizzas = pizzas;
+      // this.page = res['page'];
+      // this.totalPages = res['totalPages'];
+    });
+  }
+
+  addToCart(id: number, event: Event): void {
+    event.preventDefault();
+
+    if (!this.authService.isAuthenticated()) {
+      this.router.navigate(['/login']);
+      return;
+    }
+
+    this.cartsService.addPizza(id).subscribe(
+      res => window.location.href = "/cart",
+      err => this.errors = err
+    );
+  }
 }
